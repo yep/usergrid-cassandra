8417159058859f182853ee919b5ea65f5f610973
yep <yep171@yahoo.com>
2015-12-18 22:33:43 +0100

    posix conformant access of environment variables

--- run.sh
+++ run.sh
@@ -9,30 +9,30 @@ if [ `stat --format=%U /var/log/cassandra` != "cassandra" ] ; then
 fi
 
 # default values for configuration variables
-if [ -z "CASSANDRA_CLUSTER_NAME" ] ; then
+if [ -z "${CASSANDRA_CLUSTER_NAME}" ] ; then
   CASSANDRA_CLUSTER_NAME='usergrid'
 fi
-if [ -z "CASSANDRA_LISTEN_ADDRESS" ] ; then
+if [ -z "${CASSANDRA_LISTEN_ADDRESS}" ] ; then
   CASSANDRA_LISTEN_ADDRESS=$(hostname --ip-address)
 fi
-if [ -z "$CASSANDRA_SEEDS" ] ; then
+if [ -z "${CASSANDRA_SEEDS}" ] ; then
   CASSANDRA_SEEDS=$(hostname --ip-address)
 fi
-if [ -z "$CASSANDRA_RPC_ADDRESS" ] ; then
+if [ -z "${CASSANDRA_RPC_ADDRESS}" ] ; then
   # accept rpc requests from this address
   CASSANDRA_RPC_ADDRESS=0.0.0.0
 fi
-if [ -z "$CASSANDRA_BROADCAST_RPC_ADDRESS" ] ; then
+if [ -z "${CASSANDRA_BROADCAST_RPC_ADDRESS}" ] ; then
   # if rpc address is set to 0.0.0.0, broadcast rpc address has to be different from 0.0.0.0
   CASSANDRA_BROADCAST_RPC_ADDRESS=$(hostname --ip-address)
 fi
 
 CONFIG_FILE=/etc/cassandra/cassandra.yaml
 
-sed -i -e "s/^\(cluster_name:\).*/\1 $CASSANDRA_CLUSTER_NAME/" $CONFIG_FILE
-sed -i -e "s/^\(listen_address:\).*/\1 $CASSANDRA_LISTEN_ADDRESS/" $CONFIG_FILE
-sed -i -e "s/^\(rpc_address:\).*/\1 $CASSANDRA_RPC_ADDRESS/" $CONFIG_FILE
-sed -i -e "s/^\(# \)\(broadcast_rpc_address:\).*/\2 $CASSANDRA_BROADCAST_RPC_ADDRESS/" $CONFIG_FILE
-sed -i -e "s/^\([ ]*- seeds:\).*/\1 $CASSANDRA_SEEDS/" $CONFIG_FILE
+sed -i -e "s/^\(cluster_name:\).*/\1 ${CASSANDRA_CLUSTER_NAME/" ${CONFIG_FILE}
+sed -i -e "s/^\(listen_address:\).*/\1 ${CASSANDRA_LISTEN_ADDRESS}/" ${CONFIG_FILE}
+sed -i -e "s/^\(rpc_address:\).*/\1 ${CASSANDRA_RPC_ADDRESS}/" ${CONFIG_FILE}
+sed -i -e "s/^\(# \)\(broadcast_rpc_address:\).*/\2 ${CASSANDRA_BROADCAST_RPC_ADDRESS}/" ${CONFIG_FILE}
+sed -i -e "s/^\([ ]*- seeds:\).*/\1 ${CASSANDRA_SEEDS}/" ${CONFIG_FILE}
 
 start-stop-daemon --chuid cassandra:cassandra --exec /usr/sbin/cassandra --start -- -f
